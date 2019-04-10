66285dc04454e2e4028384b63d6afc08ab7d4de9
Peter M. Petrakis <peter.petrakis@protonmail.com>
2019-04-10 11:40:14 -0400

    removed package.sh

--- package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-conan source . --source-folder=tmp/source
-conan install  . --install-folder=tmp/build
-conan build . --source-folder=tmp/source --build-folder=tmp/build
-conan package . -bf=tmp/build -pf=tmp/package
